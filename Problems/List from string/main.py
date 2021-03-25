ccf9afacac02320c7447e25c77a2fb6da456d86f
Joel Pettifor <joelpettifor13@gmail.com>
2021-03-25 14:36:55 +0000

    Initial commit

---
+++ Problems/List from string/main.py
@@ -0,0 +1,7 @@
+# the following line reads the string to work with from the input, do not modify it, please
+input_str = input()
+
+# your code here
+def split(input_str):
+    return [char for char in input_str]
+print(split(input_str))
